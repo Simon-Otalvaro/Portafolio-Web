ea21972ad642335716d9bd7a18fccd51629a82dd
Simon-Otalvaro <Simonotalvaro6@gmail.com>
2025-03-28 15:21:46 -0500

    Commit V1

--- src/Components/Pages/Experience/Experience.jsx
+++ src/Components/Pages/Experience/Experience.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Experience.css'
 
 export const Experience = () => {
   return (
     <div>
-      <h1>EXPERIENCIA</h1>
+      <h1 className='titu'>EXPERIENCIA</h1>
+
     </div>
   )
 }
